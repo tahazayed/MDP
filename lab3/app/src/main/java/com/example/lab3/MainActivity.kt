6945ea01e6087ac1798f84801098943aab2c3a68
Taha Elsayed <tahazayed@gmail.com>
2023-04-08 16:29:10 -0500

    Add Create Account

--- lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.lab3
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.example.lab3.databinding.ActivityMainBinding
 
@@ -23,15 +25,45 @@ class MainActivity : AppCompatActivity() {
         ob.add(User("fn4", "ln4", "user4@mail.com", "123456"))
         ob.add(User("fn5", "ln5", "user5@mail.com", "123456"))
 
+        var resultContracts =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                try {
+                    // result object contains the intent and RESULT_OK or RESULT_CANCEL
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        var user = result.data?.getSerializableExtra("user") as User
+                        var userExists = false
+                        for (u in ob) {
+                            if (u.email == user!!.email) {
+                                userExists = true
+                                break
+                            }
+                        }
+                        if(!userExists){
+                            ob.add(user)
+                            Toast.makeText(this, "User added successfully you can login: ${user.email}", Toast.LENGTH_LONG).show()
+                        }
+                        else{
+                            Toast.makeText(this, "User with email ${user.email} already exists", Toast.LENGTH_LONG).show()
+                        }
+                    } else {
+                        Toast.makeText(this, "User cancelled the account creation", Toast.LENGTH_LONG).show()
+                    }
+                } catch (e: NullPointerException) {
+                    Toast.makeText(this, "NullPointerException", Toast.LENGTH_LONG)
+                }
+
+            }
+
         binding.createAccountBtn.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
 
-            startActivity(intent)
+            resultContracts.launch(intent)
         }
-        binding.loginBtn.setOnClickListener {
-            var username = binding.textEmail.text.toString()
 
-            var password = binding.textPassword.text.toString()
+        binding.loginBtn.setOnClickListener {
+            var username = binding.textEmail?.text.toString()
+
+            var password = binding.textPassword?.text.toString()
 
             var userFound = false
 
--- lab3/app/src/main/java/com/example/lab3/RegisterActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/RegisterActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lab3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lab3.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,5 +13,32 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.createAccountBtn.setOnClickListener {
+            var firstName = binding.editTextFirstName?.text.toString()
+            var lastName = binding.editTextLastName?.text.toString()
+            var email = binding.editTextEmail?.text.toString()
+            var password = binding.editTextTextPassword?.text.toString()
+            if (firstName.isNotEmpty()
+                && lastName.isNotEmpty()
+                && email.isNotEmpty()
+                && password.isNotEmpty()
+            ) {
+                var user = User(firstName, lastName, email, password)
+                val rintent = intent // getIntent()
+                val bundle = Bundle()
+                bundle.putSerializable("user", user)
+                rintent.putExtras(bundle)
+
+                setResult(Activity.RESULT_OK, rintent)
+                finish()
+            } else {
+                Toast.makeText(this, "Please fill all thr fields", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
     }
 }
