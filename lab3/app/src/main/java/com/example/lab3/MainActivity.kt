5a0309aa291d104d9060ef651e219fcfafc5a1cc
Taha Elsayed <tahazayed@gmail.com>
2023-04-08 14:58:28 -0500

    Add Shopping Category toast

--- lab3/app/src/main/java/com/example/lab3/MainActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,16 +1,51 @@
 package com.example.lab3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lab3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var ob: ArrayList<User>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Create 5 default user
+        ob = ArrayList<User>()
+        ob.add(User("fn1", "ln1", "user1@mail.com", "123456"))
+        ob.add(User("fn2", "ln2", "user2@mail.com", "123456"))
+        ob.add(User("fn3", "ln3", "user3@mail.com", "123456"))
+        ob.add(User("fn4", "ln4", "user4@mail.com", "123456"))
+        ob.add(User("fn5", "ln5", "user5@mail.com", "123456"))
+
+        binding.loginBtn.setOnClickListener {
+            var username = binding.textEmail.text.toString()
+            var password = binding.textPassword.text.toString()
+
+            var userFound = false
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                for (user in ob) {
+                    if (user.email.lowercase() == username.lowercase()
+                        && user.password == password
+                    ) {
+                        userFound = true
+                        val intent = Intent(this, ShoppingCategoryActivity::class.java)
+                        intent.putExtra("userEmail", user.email)
+                        startActivity(intent)
+                        break
+                    }
+                }
+            }
+            if (!userFound) {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
+
+
--- lab3/app/src/main/java/com/example/lab3/ShoppingCategoryActivity.kt
+++ lab3/app/src/main/java/com/example/lab3/ShoppingCategoryActivity.kt
@@ -1,15 +1,53 @@
 package com.example.lab3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.lab3.databinding.ActivityShoppingcategoryBinding
 
 class ShoppingCategoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityShoppingcategoryBinding
+    private var toast: Toast? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityShoppingcategoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val intent = this.intent
+        val userEmail = intent.getStringExtra("userEmail")
+
+        if (userEmail == null) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.textViewEmail.text = userEmail
+
+        binding.imageButtonBooks.setOnClickListener {
+            showToast("You have chosen the Books category of shopping")
+        }
+
+        binding.imageButtonClothing.setOnClickListener {
+            showToast("You have chosen the Clothing category of shopping")
+        }
+
+        binding.imageButtonBetterHealth.setOnClickListener {
+            showToast("You have chosen the Better Health category of shopping")
+        }
+
+        binding.imageButtonAyuveda.setOnClickListener {
+            showToast("You have chosen the Ayuveda category of shopping")
+        }
+
+    }
+
+    fun showToast(message: String?) {
+
+        toast?.cancel()
+
+        toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast?.show()
     }
 }
